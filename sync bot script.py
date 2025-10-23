e6f280435457b5c2a3381b708a480d6d1ea01e51
Lkshjnj <lakshyajuneja22@gmail.com>
2025-10-23 18:43:53 +0530

    updated the script, now it works, the md pushed is  disorganized tho

--- sync bot script.py
+++ sync bot script.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 from notion_client import Client
 import requests, os
+import base64
 
-load_dotenv(dotenv_path="C:\\Users\\Lakshya\\Desktop\\Random_Proj\\.env")
+load_dotenv(dotenv_path="C:\\Users\\Lakshya\\Desktop\\Random_Proj\\github sync bot\\.env")
 
 notion = Client(auth=os.getenv("NOTION_TOKEN"))
 
@@ -10,24 +11,33 @@ notion = Client(auth=os.getenv("NOTION_TOKEN"))
 db_id = os.getenv("NOTION_DATABASE_ID")
 
 data = notion.databases.query(database_id=db_id)
-tasks = [item["properties"]["name"]["title"][0]["plain_text"] for item in data["results"]]         # here the name field is not there , we have to figure out a way to make this point to the plain text.
-print("done 3")
+# print(data["results"])
+
+tasks = [
+    item["properties"]["Bug bounty checklist"]["title"][0]["plain_text"]
+    for item in data["results"]
+    if item["properties"]["Bug bounty checklist"]["title"]
+]
+
 md_content = "\n".join([f"- [ ] {t}" for t in tasks])
-print("done 4")
+
 # Push to GitHub
 repo = os.getenv("GITHUB_REPO")
-print(repo)
+# print(repo)
 token = os.getenv("GITHUB_TOKEN")
-print(token)
+# print(token)
 
+content_bytes = md_content.encode("utf-8")
 
-url = f"https://api.github.com/repos/{repo}/contents/BugBountyChecklist.md"
-headers = {"Authorization": f"token {token}"}
+url = f"https://api.github.com/repos/{repo}/contents/Bug_bounty_checklist.md"
+headers = {"Authorization": f"Bearer {token}"}
 r = requests.get(url, headers=headers)
 sha = r.json().get("sha")
-
-requests.put(url, headers=headers, json={
+res = requests.put(url, headers=headers, json={
     "message": "update checklist",
-    "content": md_content.encode("utf-8").decode("utf-8"),
+    "content": base64.b64encode(content_bytes).decode("utf-8"),
     "sha": sha
 })
+
+# print(md_content.encode("utf-8"))
+# print(res)
